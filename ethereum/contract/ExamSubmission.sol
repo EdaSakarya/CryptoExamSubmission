acf7f9d780d5f67364f5bffe4be47120594ae60a
Eda Rabiya Sakarya <sakarya.edarabiya@gmail.com>
2021-05-16 23:34:57 +0200

    - directory ethereum/contracts deleted - new Contract Address in ethereum/factory.js - new JSON Files - Contract ExamSubmission.sol configured - unused files in package.json deleted

--- ethereum/contract/ExamSubmission.sol
+++ ethereum/contract/ExamSubmission.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.8.3;
 
 contract ExamPool {
 
-    mapping (address => bool) admin;
-    mapping (address => address[]) deployedExamsOfUsers;
-    mapping (address => string[]) descriptionOfDeployedExams;
-    mapping (address => bool) profs;
-    mapping (address => bool) students;
+    mapping(address => bool) admin;
+    mapping(address => address[]) deployedExamsOfUsers;
+    mapping(address => string[]) descriptionOfDeployedExams;
+    mapping(address => bool) profs;
+    mapping(address => bool) students;
     enum UserType{Professor, Student}
 
     // modifirer for restriction to address of Admin
@@ -14,11 +14,14 @@ contract ExamPool {
         require(admin[msg.sender]);
         _;
     }
-    function createExam(string memory _description, string memory _typeOfWork, uint8 _typeOfSubmission, string memory _subject, uint  _submissionTime, address [] memory _student, address []memory _professor) public restrictedToAdmin {
+    function createExam(string memory _description, string memory _typeOfWork, uint8 _typeOfSubmission,
+        string memory _subject, uint _submissionTime, address [] memory _student, address [] memory _professor)
+    public restrictedToAdmin {
         require(profs[_professor[0]] && students[_student[0]]);
-        ExamSubmission newExamAddress = new ExamSubmission(_description, _typeOfWork, _typeOfSubmission, _subject, _submissionTime, _student, _professor);
+        ExamSubmission newExamAddress = new ExamSubmission(_description, _typeOfWork, _typeOfSubmission, _subject,
+            _submissionTime, _student, _professor);
 
-        for(uint8 prof = 0; prof < _professor.length; prof++)
+        for (uint8 prof = 0; prof < _professor.length; prof++)
         {
             address[] storage deployedExamsOfProf = deployedExamsOfUsers[_professor[prof]];
             deployedExamsOfProf.push(address(newExamAddress));
@@ -26,7 +29,7 @@ contract ExamPool {
             string []storage descriptionOfProfs = descriptionOfDeployedExams[_professor[prof]];
             descriptionOfProfs.push(_description);
         }
-        for(uint8 student = 0; student < _student.length; student++)
+        for (uint8 student = 0; student < _student.length; student++)
         {
             address[] storage deployedExamsOfStudents = deployedExamsOfUsers[_student[student]];
             deployedExamsOfStudents.push(address(newExamAddress));
@@ -36,29 +39,30 @@ contract ExamPool {
         // descriptionOfDeployedExams[address(newExamAddress)]= _description;
     }
 
-    function getExamsOfUser(address _user) public view returns(address[] memory) {
+    function getExamsOfUser(address _user) public view returns (address[] memory) {
         return deployedExamsOfUsers[_user];
     }
 
-    function getExamsDescriptionsOfUser(address _user) public view returns(string[] memory) {
+    function getExamsDescriptionsOfUser(address _user) public view returns (string[] memory) {
         return descriptionOfDeployedExams[_user];
     }
 
     function createUser(int8 _type, address _userAddress) public restrictedToAdmin {
         require(!profs[_userAddress] && !students[_userAddress]);
-        if(UserType(_type) == UserType.Professor){
+        if (UserType(_type) == UserType.Professor) {
             profs[_userAddress] = true;
 
-        }else if (UserType(_type) == UserType.Student) {
+        } else if (UserType(_type) == UserType.Student) {
             students[_userAddress] = true;
         }
     }
 
-    function createAdmin() public{
+    function createAdmin() public {
         admin[msg.sender] = true;
     }
 
 }
+
 contract ExamSubmission {
 
     struct ExamDetails {
@@ -72,21 +76,21 @@ contract ExamSubmission {
         address[] student;
         address[] prof;
         ExamStatus status;
-        mapping (uint => uint) timestamp;
+        mapping(uint => uint) timestamp;
         Data data;
     }
 
     struct Data {
         string nameOfData;
-        // string can be a link or a ipfshash
         string ipfshashORlink;
     }
 
     ExamDetails exam;
     Data fileData;
-    enum ExamStatus { ToSubmit, Submitted, InCorrection, Corrected }
-    enum ExamTimestamp { CreatedOn, UploadedOn, InCorrectionOn, GradedOn, CommentedOn }
-    enum TypeOfSubmission { Upload, Link }
+
+    enum ExamStatus {ToSubmit, Submitted, InCorrection, Corrected}
+    enum ExamTimestamp {CreatedOn, UploadedOn, InCorrectionOn, GradedOn, CommentedOn}
+    enum TypeOfSubmission {Upload, Link}
 
 
     // modifier for restriction to address of Professor
@@ -100,14 +104,16 @@ contract ExamSubmission {
         require(msg.sender == exam.student[0] || msg.sender == exam.student[1]);
         _;
     }
-
-    constructor(string memory _description, string memory _typeOfWork, uint8 _typeOfSubmission, string memory _subject, uint _submissionTime, address[] memory _student, address[] memory _professor) public {
+    //  constructor
+    constructor(string memory _description, string memory _typeOfWork, uint8 _typeOfSubmission,
+        string memory _subject, uint _submissionTime, address[] memory _student, address[] memory _professor) public {
         createExam(_description, _typeOfWork, _typeOfSubmission, _subject, _submissionTime, _student, _professor);
     }
 
     // function create a new Exam
-    function createExam(string memory _description, string memory _typeOfWork, uint8 _typeOfSubmission, string memory _subject, uint _submissionTime, address[] memory _student, address[] memory _prof) private {
-        ExamDetails storage newExam= exam;
+    function createExam(string memory _description, string memory _typeOfWork, uint8 _typeOfSubmission,
+        string memory _subject, uint _submissionTime, address[] memory _student, address[] memory _prof) private {
+        ExamDetails storage newExam = exam;
 
         newExam.description = _description;
         newExam.timestamp[uint(ExamTimestamp.CreatedOn)] = block.timestamp;
@@ -128,18 +134,19 @@ contract ExamSubmission {
 
     /* get Functions */
     // function returns address of student
-    function getStudent() public view returns(address[] memory) {
+    function getStudent() public view returns (address[] memory) {
         return exam.student;
     }
 
     // function returns address of Professor
-    function getProf() public view returns(address[] memory) {
+    function getProf() public view returns (address[] memory) {
         return exam.prof;
     }
 
     // function returns a getSummary of the exam
-    function getDetailsOfExam() public view returns(string memory, string memory, string memory,uint, uint, uint, string memory, address[] memory, address[] memory, ExamStatus){
-        return(
+    function getDetailsOfExam() public view returns (string memory, string memory, string memory, uint, uint, uint,
+        string memory, address[] memory, address[] memory, ExamStatus){
+        return (
         exam.description,
         exam.subject,
         exam.typeOfWork,
@@ -155,7 +162,7 @@ contract ExamSubmission {
 
     // function returns all timestamps
     function getTimestampOfExamProcess() public view returns (uint, uint, uint, uint){
-        return(
+        return (
         exam.timestamp[0],
         exam.timestamp[1],
         exam.timestamp[2],
@@ -173,10 +180,11 @@ contract ExamSubmission {
     function submitExam(string memory _name, string memory _input) restrictedToStudent public {
         ExamDetails storage newExam = exam;
         require(newExam.submissionTime > block.timestamp);
-        require(newExam.status == ExamStatus.ToSubmit || (newExam.status != ExamStatus.InCorrection && newExam.status != ExamStatus.Corrected));
+        require(newExam.status == ExamStatus.ToSubmit ||
+            (newExam.status != ExamStatus.InCorrection && newExam.status != ExamStatus.Corrected));
         Data memory data = Data({
-        nameOfData:_name,
-        ipfshashORlink: _input
+        nameOfData : _name,
+        ipfshashORlink : _input
         });
         fileData = data;
         newExam.data = data;
@@ -184,7 +192,8 @@ contract ExamSubmission {
         newExam.timestamp[uint(ExamTimestamp.UploadedOn)] = block.timestamp;
         setStatusOfExam(ExamStatus.Submitted);
     }
-    function getTypeOfSubmit() restrictedToStudent restrictedToProf public view returns(uint){
+
+    function getTypeOfSubmit() restrictedToStudent restrictedToProf public view returns (uint){
         ExamDetails storage newExam = exam;
         return newExam.typeOfSubmission;
     }
@@ -197,13 +206,13 @@ contract ExamSubmission {
     // function allows prof to write a comment
     function setComment(string memory _comment) private restrictedToProf {
         require(exam.status != ExamStatus.ToSubmit);
-        ExamDetails storage newExam= exam;
+        ExamDetails storage newExam = exam;
         newExam.timestamp[uint(ExamTimestamp.CommentedOn)] = block.timestamp;
         newExam.comment = _comment;
     }
 
     // function allows to download the exam file
-    function downloadExam() public view returns(string memory) {
+    function downloadExam() public view returns (string memory) {
         ExamDetails storage newExam = exam;
         require(newExam.status != ExamStatus.ToSubmit);
         Data memory data = newExam.data;
@@ -212,9 +221,9 @@ contract ExamSubmission {
 
     // function allows professor to set a Grade
     // status changes from 'inCorrection' to 'corrected'
-    function setGradeAndComment(uint _grade, string memory _comment ) public restrictedToProf {
+    function setGradeAndComment(uint _grade, string memory _comment) public restrictedToProf {
         require(exam.status == ExamStatus.InCorrection || exam.status == ExamStatus.Corrected);
-        ExamDetails storage newExam= exam;
+        ExamDetails storage newExam = exam;
         newExam.timestamp[uint(ExamTimestamp.GradedOn)] = block.timestamp;
         newExam.grade = _grade;
         newExam.timestamp[uint(ExamTimestamp.CommentedOn)] = block.timestamp;
@@ -223,7 +232,7 @@ contract ExamSubmission {
     }
 
     // function allows professor to set the exam in Correction (in Frontend it should be a button)
-    function setStatusInCorrection() public restrictedToProf{
+    function setStatusInCorrection() public restrictedToProf {
         ExamDetails storage newExam = exam;
         require(newExam.status == ExamStatus.Submitted);
         newExam.status = ExamStatus.InCorrection;
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,6 +2,5 @@ import web3 from './web3';
 import ExamPool from './build/ExamPool.json';
 
 //contract version 0.8.3
-const instance = new web3.eth.Contract(ExamPool.abi, '0x40CB29641E1F1CC25114Adb928E0f278E334f556');
-// const instance = new web3.eth.Contract(ExamPool.abi, '0xDA36fDA08E6E00dDA10c657fed99615B454C2e14');
+const instance = new web3.eth.Contract(ExamPool.abi, '0x2443cC910F001074F2Ba2C95892a8B1E0a4b013E');
 export default instance;
